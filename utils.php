dc5490d2afd1c69125ea48c1512732bcd110168d
pedropamn <pedro-pamn@hotmail.com>
2018-12-18 10:09:28 -0200

    Update utils.php

--- utils.php
+++ utils.php
@@ -90,6 +90,14 @@ date_default_timezone_set('America/Sao_Paulo');
 		$temp = str_replace('/','-',$data); 
 		return date("Y-m-d H:i:s",strtotime($temp)); //Data no formate DATE para o MySql
 	}
+
+	//Diferença entre dois datetimes, no formato yyyy-mm-dd hh:mm (formato Mysql). Retorna no formato '1d 1h 1m	'
+	function dif_entre_datetimes($dt1,$dt2){
+		$datetime1 = new DateTime($dt1);
+		$datetime2 = new DateTime($dt2);
+		$interval = $datetime1->diff($datetime2);
+		return $interval->format('%dd %hh %im');
+	}
 	
 	//Calcula a diferença entre hoje e uma determinada data. Retorna resultados negativos, zero ou positivos, estes dois últimos com o sinal de '+'
 	function data_dif($data){
@@ -134,6 +142,19 @@ date_default_timezone_set('America/Sao_Paulo');
 		$tts = file_get_contents("https://translate.google.com/translate_tts?ie=UTF-8&q=".$txt."?&tl=".$lang."&client=tw-ob");
 	}
 
+      //Valida MIME e extensão
+	function valida_mime_ext($mime, $ext){
+		$allowed_mime = ['application/pdf','image/jpeg','image/png','video/mpeg','video/mp4','text/html','application/octet-stream'];
+		$allowed_ext = ['pdf','jpg','png','mpeg','mp4','html'];
+		if(in_array($mime,$allowed_mime) && in_array($ext,$allowed_ext)){
+			return 'true';
+		}
+		else{
+			return 'false';
+
+		}
+	}
+
       //Gerencia um upload com validação de extensão e MIME (Testado com Dropzone.js)
       function get_upload($uploaddir){
       	$uploaddir = './pdf/';
@@ -154,12 +175,118 @@ date_default_timezone_set('America/Sao_Paulo');
 	   }
       }
 	
-	//Evita cache
+   //Evita cache
     function no_cache(){
     	header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 	header("Cache-Control: post-check=0, pre-check=0", false);
 	header("Pragma: no-cache");
     }
+
+	//Criptografa senha ao cadastrar usuário
+	function cripto_senha($senha){
+		$hash_gerado = password_hash($senha, PASSWORD_BCRYPT);
+		return $hash_gerado;
+	}
+
+	//Verifica senha em texto com o hash
+	function verifica_senha($senha, $hash){
+		if (password_verify($senha, $hash)) {
+			return 'true';
+		} else {
+			return 'false';
+		}
+	}
+
+	//String aleatória
+	function generateRandomString($length = 10) {
+		$characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+		$charactersLength = strlen($characters);
+		$randomString = '';
+		for ($i = 0; $i < $length; $i++) {
+			$randomString .= $characters[rand(0, $charactersLength - 1)];
+		}
+		return $randomString;
+	}
+
+	//Checa o tempo de login. Desloga se ultrapassar o tempo limite
+	function check_tempo_login(){
+		
+		$agora = now();
+		$limite_minutos = 10;
+		session_start();
+		if (isset($_SESSION['LAST_ACTIVITY'])){			
+			$datetime1 = new DateTime($agora);
+			$datetime2 = new DateTime($_SESSION['LAST_ACTIVITY']);
+			$interval = $datetime1->diff($datetime2);
+			//return $interval->format('%dd %hh %im');
+			$dif = $interval->format('%i'); // minutos
+			//die($dif);
+			if($dif >= $limite_minutos){
+				session_start();
+				session_destroy();
+				header("Location: ".'http://'.$_SERVER['HTTP_HOST'].'/login');
+			}
+			else{
+				$_SESSION['LAST_ACTIVITY'] = $agora;
+			}
+		}
+		
+	}
+
+	//Obtém variáveis $_SERVER
+	function get_server_vars(){
+		$indicesServer = array('PHP_SELF', 
+		'argv', 
+		'argc', 
+		'GATEWAY_INTERFACE', 
+		'SERVER_ADDR', 
+		'SERVER_NAME', 
+		'SERVER_SOFTWARE', 
+		'SERVER_PROTOCOL', 
+		'REQUEST_METHOD', 
+		'REQUEST_TIME', 
+		'REQUEST_TIME_FLOAT', 
+		'QUERY_STRING', 
+		'DOCUMENT_ROOT', 
+		'HTTP_ACCEPT', 
+		'HTTP_ACCEPT_CHARSET', 
+		'HTTP_ACCEPT_ENCODING', 
+		'HTTP_ACCEPT_LANGUAGE', 
+		'HTTP_CONNECTION', 
+		'HTTP_HOST', 
+		'HTTP_REFERER', 
+		'HTTP_USER_AGENT', 
+		'HTTPS', 
+		'REMOTE_ADDR', 
+		'REMOTE_HOST', 
+		'REMOTE_PORT', 
+		'REMOTE_USER', 
+		'REDIRECT_REMOTE_USER', 
+		'SCRIPT_FILENAME', 
+		'SERVER_ADMIN', 
+		'SERVER_PORT', 
+		'SERVER_SIGNATURE', 
+		'PATH_TRANSLATED', 
+		'SCRIPT_NAME', 
+		'REQUEST_URI', 
+		'PHP_AUTH_DIGEST', 
+		'PHP_AUTH_USER', 
+		'PHP_AUTH_PW', 
+		'AUTH_TYPE', 
+		'PATH_INFO', 
+		'ORIG_PATH_INFO') ; 
+
+		echo '<table cellpadding="10">' ; 
+		foreach ($indicesServer as $arg) { 
+			if (isset($_SERVER[$arg])) { 
+				echo '<tr><td>'.$arg.'</td><td>' . $_SERVER[$arg] . '</td></tr>' ; 
+			} 
+			else { 
+				echo '<tr><td>'.$arg.'</td><td>-</td></tr>' ; 
+			} 
+		} 
+		echo '</table>' ; 
+	}
 
 	
 ?>
