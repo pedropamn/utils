023e9bfdb3916f00aacbf38322076e053aa6be44
Pedro Neto <19939247+pedropamn@users.noreply.github.com>
2022-08-07 14:10:46 -0300

    Update utils.js

--- utils.js
+++ utils.js
@@ -42,3 +42,18 @@ function Busca() {
 //Obter parâmetros GET
 var urlParams = new URLSearchParams(window.location.search);
 var test = urlParams.get('test'); //index.php?test=123
+
+
+//Diff entre datas
+function parseDate(str) {
+    var mdy = str.split('/');
+    return new Date(mdy[2], mdy[1], mdy[0]);
+}
+
+function datediff(first, second) {
+    // Take the difference between the dates and divide by milliseconds per day.
+    // Round to nearest whole number to deal with DST.
+    return Math.round((second-first)/(1000*60*60*24));
+}
+
+datediff(parseDate('01/01/2022'), parseDate('03/01/2022'));
